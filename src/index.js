9d6fb0ab7b8cf20c124575193820598aead98a6a
fromtheplace <blamboxcity@gmail.com>
2025-07-10 12:36:41 +1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme'; // Make sure theme.js or theme.ts exports your MUI theme
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
